3b8885a4055b708e9d00fa2e1054d8d8ca090084
Ken Knight <visiperl@gmail.com>
2016-08-09 18:16:18 -0400

    Delete pics and add new on index

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -2,10 +2,10 @@ class PicsController < ApplicationController
   before_action :find_pic, only: [:show, :edit, :update, :destroy]
 
   def index
+    @pics = Pic.all.order("created_at DESC")
   end
 
   def show
-
 
   end
 
@@ -23,6 +23,23 @@ class PicsController < ApplicationController
     end
 
   end
+
+  def edit
+  end
+
+  def update
+    if @pic.update(pic_params)
+      redirect_to @pic, notice: "Congrats!  Pic was updated!"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @pic.destroy
+    redirect_to root_path
+  end
+
 
 
 
